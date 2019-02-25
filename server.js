6617f683a7df37407c8b66b19205c22bbef3782f
Ali Usman <ali.usman@indusvalleylabs.com>
2019-02-25 17:00:11 +0500

    Made port variable Now using environment variables

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const helpers = require('./helpers');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 function registerHelpers(obj) {
 	Object.keys(obj).forEach(key => hbs.registerHelper(key, obj[key]));
 }
@@ -50,6 +52,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000');
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
